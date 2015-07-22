5903b7da2e8aae8456c628de0e5f8c96e5d068f5
noprom <tyee.noprom@qq.com>
2015-07-22 12:13:33 +0800

    Initial Commit

---
+++ Dianle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dianle
+//
+//  Created by noprom on 15/7/22.
+//  Copyright (c) 2015年 noprom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dianle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dianle
+//
+//  Created by noprom on 15/7/22.
+//  Copyright (c) 2015年 noprom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
